e9f0e6c01a47f831e861e84ef52c4886eddf4bfe
Aamir Ali <125216382+AamirAliJatoi@users.noreply.github.com>
2023-08-29 10:25:42 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Aamir Ali Jatoi",
+  name: "Aamir-Ali Jatoi",
   title: "Graphics` Designer and 3D Modeler",
   email: "aamir.ali@studentambassadors.com",
   gitHub: "AamirAliJatoi",
